fc936b3f15420070ffa0425521f345b749549c95
Tulir Asokan <tulir@maunium.net>
2017-05-06 17:19:29 +0300

    Fix Ender-Infused Diamond tile name

--- src/net/maunium/energeticshielding/blocks/EnderInfusedDiamondBlock.java
+++ src/net/maunium/energeticshielding/blocks/EnderInfusedDiamondBlock.java
@@ -9,7 +9,7 @@ public class EnderInfusedDiamondBlock extends Block {
 
 	protected EnderInfusedDiamondBlock() {
 		super(Material.rock);
-		this.setBlockName("blockEnderInfusedDiamond");
+		this.setBlockName("blockEnderDiamond");
 		this.setBlockTextureName(EnergeticShielding.MODID + ":ender_infused_diamond_block");
 		this.setCreativeTab(EnergeticShielding.tab);
 		this.setHardness(5.0F);
