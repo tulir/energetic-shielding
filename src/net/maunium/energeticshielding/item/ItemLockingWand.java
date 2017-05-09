7ab6f798df7b098b51c4f9e00689bd35d0f78134
Tulir Asokan <tulir@maunium.net>
2017-05-09 13:31:12 +0300

    Display charge in locking wand lore

--- src/net/maunium/energeticshielding/item/ItemLockingWand.java
+++ src/net/maunium/energeticshielding/item/ItemLockingWand.java
@@ -29,7 +29,8 @@ public class ItemLockingWand extends Item implements IEnergyContainerItem {
 
 	public ItemLockingWand() {
 		super();
-		setHasSubtypes(true)
+		this
+				.setHasSubtypes(true)
 				.setUnlocalizedName("lockingWand")
 				.setCreativeTab(EnergeticShielding.tab)
 				.setMaxStackSize(1)
@@ -37,12 +38,20 @@ public class ItemLockingWand extends Item implements IEnergyContainerItem {
 	}
 
 	@Override
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean bool) {
+		int maxEnergy = this.getMaxEnergyStored(stack);
+		int energy = this.getEnergyStored(stack);
+		list.add(String.format("Charge: %d / %d RF", energy, maxEnergy));
+	}
+
+	@Override
 	public void registerIcons(IIconRegister reg) {
 		String textureName = EnergeticShielding.texture("locking_wand");
-		icons[0] = reg.registerIcon(textureName + "_coreless");
-		icons[1] = reg.registerIcon(textureName + "_tier1");
-		icons[2] = reg.registerIcon(textureName + "_tier2");
-		icons[3] = reg.registerIcon(textureName + "_tier3");
+		this.icons[0] = reg.registerIcon(textureName + "_coreless");
+		this.icons[1] = reg.registerIcon(textureName + "_tier1");
+		this.icons[2] = reg.registerIcon(textureName + "_tier2");
+		this.icons[3] = reg.registerIcon(textureName + "_tier3");
 	}
 
 	@Override
@@ -52,13 +61,13 @@ public class ItemLockingWand extends Item implements IEnergyContainerItem {
 
 	@Override
 	public IIcon getIconFromDamage(int meta) {
-		return icons[meta % 4];
+		return this.icons[meta % 4];
 	}
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	public void getSubItems(Item item, CreativeTabs tab, List l) {
-		for (int i = 0; i < icons.length; i++) {
+		for (int i = 0; i < this.icons.length; i++) {
 			l.add(new ItemStack(item, 1, i));
 		}
 	}
@@ -121,7 +130,7 @@ public class ItemLockingWand extends Item implements IEnergyContainerItem {
 		if (stack.getItemDamage() == 0) {
 			return 0;
 		}
-		return getMaxEnergyStored(stack) - getEnergyStored(stack);
+		return this.getMaxEnergyStored(stack) - this.getEnergyStored(stack);
 	}
 
 	@Override
@@ -129,7 +138,7 @@ public class ItemLockingWand extends Item implements IEnergyContainerItem {
 		if (stack.getItemDamage() == 0) {
 			return 0;
 		}
-		return getMaxEnergyStored(stack);
+		return this.getMaxEnergyStored(stack);
 	}
 
 	@Override
@@ -143,8 +152,8 @@ public class ItemLockingWand extends Item implements IEnergyContainerItem {
 			container.setTagCompound(new NBTTagCompound());
 		}
 		int energy = container.getTagCompound().getInteger("Energy");
-		int energyReceived = Math.min(getMaxEnergyStored(container) - energy,
-				Math.min(getMaxReceive(container), maxReceive));
+		int energyReceived = Math.min(this.getMaxEnergyStored(container) - energy,
+				Math.min(this.getMaxReceive(container), maxReceive));
 
 		if (!simulate) {
 			energy += energyReceived;
@@ -173,7 +182,7 @@ public class ItemLockingWand extends Item implements IEnergyContainerItem {
 		if (!stack.hasTagCompound()) {
 			stack.setTagCompound(new NBTTagCompound());
 		}
-		int maxRadius = getMaxRadius(stack);
+		int maxRadius = this.getMaxRadius(stack);
 		if (radius > maxRadius) {
 			radius = maxRadius;
 		}
@@ -187,9 +196,9 @@ public class ItemLockingWand extends Item implements IEnergyContainerItem {
 		if (w.isRemote || p.isSneaking()) {
 			return false;
 		}
-		int radius = getRadius(stack);
-		if (useEnergy(stack, radius * 5000 - stack.getItemDamage() % 4 * 1000)) {
-			protect(stack, p, w, x, y, z, side);
+		int radius = this.getRadius(stack);
+		if (this.useEnergy(stack, radius * 5000 - stack.getItemDamage() % 4 * 1000)) {
+			this.protect(stack, p, w, x, y, z, side);
 			return true;
 		}
 		return false;
@@ -204,7 +213,7 @@ public class ItemLockingWand extends Item implements IEnergyContainerItem {
 		TileEntity currentTile = world.getTileEntity(x, y, z);
 		boolean solid = world.isBlockNormalCubeDefault(x, y, z, true);
 		if (currentTile == null && solid) {
-			List<BlockPosition> blocks = getBlocksInRadius(stack, x, y, z, ForgeDirection.getOrientation(side));
+			List<BlockPosition> blocks = this.getBlocksInRadius(stack, x, y, z, ForgeDirection.getOrientation(side));
 
 			int[] wandFriends = ItemIdentityCard.getFriendHashes(stack, player);
 
@@ -228,7 +237,8 @@ public class ItemLockingWand extends Item implements IEnergyContainerItem {
 		} else if (currentTile != null && currentTile instanceof TileProtected) {
 			TileProtected currentTileProtected = (TileProtected) currentTile;
 			if (currentTileProtected.canBeEditedBy(player)) {
-				List<BlockPosition> blocks = getBlocksInRadius(stack, x, y, z, ForgeDirection.getOrientation(side));
+				List<BlockPosition> blocks = this.getBlocksInRadius(stack, x, y, z,
+						ForgeDirection.getOrientation(side));
 				for (BlockPosition pos : blocks) {
 					TileProtected tile = pos.getTileEntity(world, TileProtected.class);
 					if (tile != null && tile.canBeEditedBy(player)) {
@@ -243,7 +253,7 @@ public class ItemLockingWand extends Item implements IEnergyContainerItem {
 
 	public List<BlockPosition> getBlocksInRadius(ItemStack stack, int centerX, int centerY, int centerZ,
 			ForgeDirection side) {
-		int radius = getRadius(stack) - 1;
+		int radius = this.getRadius(stack) - 1;
 		List<BlockPosition> blocks = new ArrayList<>();
 		if (side == ForgeDirection.DOWN || side == ForgeDirection.UP) {
 			for (int x = centerX - radius; x <= centerX + radius; x++) {
@@ -270,13 +280,13 @@ public class ItemLockingWand extends Item implements IEnergyContainerItem {
 	@Override
 	public ItemStack onItemRightClick(ItemStack stack, World w, EntityPlayer p) {
 		if (!w.isRemote && p.isSneaking()) {
-			int radius = getRadius(stack);
-			int maxRadius = getMaxRadius(stack);
+			int radius = this.getRadius(stack);
+			int maxRadius = this.getMaxRadius(stack);
 			int newRadius;
 			if (radius < maxRadius) {
-				newRadius = setRadius(stack, radius + 1);
+				newRadius = this.setRadius(stack, radius + 1);
 			} else {
-				newRadius = setRadius(stack, 1);
+				newRadius = this.setRadius(stack, 1);
 			}
 			if (newRadius != radius) {
 				p.addChatMessage(new ChatComponentText("Radius: " + newRadius + " (max: " + maxRadius + ")"));
