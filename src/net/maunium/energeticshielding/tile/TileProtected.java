0fa3ad7c75cffede2b7bc4c411d84b256f155592
Tulir Asokan <tulir@maunium.net>
2017-05-15 14:01:41 +0300

    Remove owner conversion

--- src/net/maunium/energeticshielding/tile/TileProtected.java
+++ src/net/maunium/energeticshielding/tile/TileProtected.java
@@ -4,15 +4,11 @@ import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
-
-import net.minecraftforge.common.util.Constants;
 
 public class TileProtected extends BaseTile {
 	public int[] owners = {};
 	public Block block = Blocks.air;
 	public byte blockMeta = 0;
-	public boolean safeToRemove = false;
 	public byte light;
 
 	@Override
@@ -34,17 +30,7 @@ public class TileProtected extends BaseTile {
 		this.block = (Block) Block.blockRegistry.getObject(tag.getString("BlockName"));
 		this.blockMeta = tag.getByte("BlockMeta");
 		this.light = tag.getByte("BlockLight");
-		this.owners = tag.getIntArray("OwnerHashes");
-		if (this.owners.length == 0) {
-			NBTTagList tags = tag.getTagList("Owners", Constants.NBT.TAG_STRING);
-			this.owners = new int[tags.tagCount()];
-			for (int i = 0; i < tags.tagCount(); i++) {
-				String uuid = tags.getStringTagAt(i);
-				if (uuid != null) {
-					this.owners[i] = uuid.hashCode();
-				}
-			}
-		}
+		this.owners = tag.getIntArray("Owners");
 		if (this.block == null) {
 			this.block = Blocks.stone;
 		}
@@ -55,6 +41,6 @@ public class TileProtected extends BaseTile {
 		tag.setString("BlockName", Block.blockRegistry.getNameForObject(this.block));
 		tag.setByte("BlockMeta", this.blockMeta);
 		tag.setByte("BlockLight", this.light);
-		tag.setIntArray("OwnerHashes", this.owners);
+		tag.setIntArray("Owners", this.owners);
 	}
 }
